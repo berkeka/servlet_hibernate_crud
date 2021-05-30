40ab3dc34329bf83cb271eba005772592e03c7ab
berkeka <berkekalkantr@gmail.com>
2021-05-30 08:40:58 +0300

    Fix minor issue
    Added new parameters to mysql connection string. Turkish timezone caused
    an error.

--- src/main/java/assignment_6/utl/HibernateUtil.java
+++ src/main/java/assignment_6/utl/HibernateUtil.java
@@ -20,7 +20,7 @@ public class HibernateUtil {
 				
 				Properties settings = new Properties();
 				settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
-				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/assignment_6?useSSL=false");
+				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/assignment_6?useSSL=false&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
 				settings.put(Environment.USER, "root");
 				settings.put(Environment.PASS, "root");
 				settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
